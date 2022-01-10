2b2e2a74d00b38c631fab83237b8457eaa8b3821
Nicolas Hoecker <benjamin.nicolas@gmail.com>
2022-01-10 14:43:43 +0100

    added export to networkx graph from neo4j query

--- neural-subgraph-matching/ldbcgeneration.py
+++ neural-subgraph-matching/ldbcgeneration.py
@@ -1,4 +1,6 @@
 from neo4j import GraphDatabase
+from utils import graph_from_cypher, saveGraph, loadGraph
+import networkx as nx
 
 class HelloWorldExample:
 
@@ -11,7 +13,7 @@ class HelloWorldExample:
     def get_target_graph(self):
         with self.driver.session() as session:
             uniquePersons = session.read_transaction(self.outerQuery)
-            session.read_transaction(self.innerQuery, uniquePersons)
+            return session.read_transaction(self.innerQuery, uniquePersons)
 
     @staticmethod
     def outerQuery(tx):
@@ -21,15 +23,32 @@ class HelloWorldExample:
             values.append(record.value())
         return values
 
+
     @staticmethod
     def innerQuery(tx,person_ids):
+        resultContainer = []
         for id in person_ids:
             result = tx.run("MATCH (p:person)-[r:KNOWS]->(p2:person) WHERE p.person_id = $id RETURN * LIMIT 3",id = id)
+            resultContainer.append(graph_from_cypher(result))
             for record in result:
                 print(record)
+        return resultContainer
+## TODO:  Question: Is it necessary to separarate test and train set in terms of no overlapping person_ids
 
 
 if __name__ == "__main__":
     greeter = HelloWorldExample("bolt://localhost:7687", "neo4j", "1234")
     result = greeter.get_target_graph()
     greeter.close()
+    #print(result)
+    #print(type(result))
+    sample_id = 0
+    for subgraph in result:
+        saveGraph("train",subgraph,str(sample_id))
+        sample_id =+ 1
+    #    print(subgraph)
+    #    print(nx.density(subgraph))
+    #    print(nx.to_dict_of_dicts(subgraph))
+
+    testGraph = loadGraph("train", str(0))
+    print(testGraph)
