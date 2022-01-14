55783615fce407048fad2e4c68d94dbd6157e63e
Nicolas Hoecker <benjamin.nicolas@gmail.com>
2022-01-14 11:34:24 +0100

    updated target queries extended

--- neural-subgraph-matching/common/ldbc/utils.py
+++ neural-subgraph-matching/common/ldbc/utils.py
@@ -4,22 +4,43 @@ import os
 
 # Source from https://stackoverflow.com/questions/59289134/constructing-networkx-graph-from-neo4j-query-result
 
+"""
+edge features
+edge feature 1: KNOWS -> 0, IS_LOCATED_IN -> 1
 
-def graph_from_cypher(results):
+
+node features
+node feature 1: female person -> 0, male person -> 1, place -> 2
+
+"""
+
+def graph_from_cypher(results, useFeatures = False):
     G = nx.Graph()
     nodes = list(results.graph()._nodes.values())
-    for node in nodes:
-        G.add_node(node.id,  # , labels=node._labels, properties=node._properties
-                   )
-    rels = list(results.graph()._relationships.values())
-    for rel in rels:
-        G.add_edge(rel.start_node.id, rel.end_node.id
-        #,key=rel.id, type=rel.type, properties=rel._properties
-        )
-        #G.add_edge(rel.end_node.id,rel.start_node.id
-        #,key=rel.id, type=rel.type, properties=rel._properties
-        #)
-    return G
+
+    if !useFeatures:
+        for node in nodes:
+            G.add_node(node.id)
+        rels = list(results.graph()._relationships.values())
+        for rel in rels:
+            G.add_edge(rel.start_node.id, rel.end_node.id)
+        return G
+    else:
+        for node in nodes:
+            G.add_node(node.id,  # , labels=node._labels, properties=node._properties
+                       )
+        rels = list(results.graph()._relationships.values())
+        for rel in rels:
+            G.add_edge(rel.start_node.id, rel.end_node.id
+            #,key=rel.id, type=rel.type, properties=rel._properties
+            )
+            #G.add_edge(rel.end_node.id,rel.start_node.id
+            #,key=rel.id, type=rel.type, properties=rel._properties
+            #)
+        return G
+
+def node2feature(node):
+
 
 
 def saveGraph(setName, graph, name):
--- neural-subgraph-matching/ldbcGenerator.py
+++ neural-subgraph-matching/ldbcGenerator.py
@@ -2,7 +2,8 @@ from neo4j import GraphDatabase
 from common.ldbc.utils import graph_from_cypher, saveGraph, loadGraph
 import networkx as nx
 
-class LdbcGeneratorWithoutFeatures:
+
+class LdbcGenerator:
 
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
@@ -10,10 +11,10 @@ class LdbcGeneratorWithoutFeatures:
     def close(self):
         self.driver.close()
 
-    def get_target_graph(self):
+    def get_target_graph(self, useFeatures = False):
         with self.driver.session() as session:
             uniquePersons = session.read_transaction(self.outerQuery)
-            return session.read_transaction(self.innerQuery, uniquePersons)
+            return session.read_transaction(self.innerQuery, uniquePersons, useFeatures = useFeatures)
 
     @staticmethod
     def outerQuery(tx):
@@ -28,17 +29,22 @@ class LdbcGeneratorWithoutFeatures:
 
 
     @staticmethod
-    def innerQuery(tx,person_ids):
+    def innerQuery(tx,person_ids, useFeatures):
         resultContainer = []
         for id in person_ids:
-            result = tx.run("MATCH (p1:person)-[r:KNOWS*1..2]->(p2:person) WHERE p1.person_id = $id RETURN *",id = id)
-            resultContainer.append(graph_from_cypher(result))
+            #result = tx.run("MATCH (p1:person)-[r:KNOWS*1..2]->(p2:person) WHERE p1.person_id = $id RETURN *",id = id)
+            result = tx.run("MATCH (place1:place)<-[l1:IS_LOCATED_IN]-(p1:person)-[r:KNOWS*1..2]->(p2:person)-[l2:IS_LOCATED_IN]->(place2:place) WHERE p1.person_id = $id RETURN *",id = id)
+            #for record in result:
+            #    print(record)
+            #return
+            resultContainer.append(graph_from_cypher(result, useFeatures = useFeatures))
         return resultContainer
 ## TODO:  Question: Is it necessary to separarate test and train set in terms of no overlapping person_ids
 
 if __name__ == "__main__":
-    generator = LdbcGeneratorWithoutFeatures("bolt://localhost:7687", "neo4j", "1234")
-    targetGraphs = generator.get_target_graph()
+
+    generator = LdbcGenerator("bolt://localhost:7687", "neo4j", "1234")
+    targetGraphs = generator.get_target_graph(useFeatures=True)
     generator.close()
 
     train_set= len(targetGraphs)  * 0.8
@@ -47,7 +53,6 @@ if __name__ == "__main__":
             saveGraph("train",targetGraph,str(graphId))
         else:
             saveGraph("test",targetGraph,str(graphId))
-
 
 
 
